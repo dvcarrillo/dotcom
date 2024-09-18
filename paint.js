df4f87815c95d45774c1f543571415af0824f2c1
David Vargas Carrillo <dv.carrillo@yahoo.com>
2024-09-18 22:14:17 +0200

    Included fun drawing stuff

--- paint.js
+++ paint.js
@@ -1,3 +1,8 @@
+var R = 162;
+var G = 250;
+var B = 163;
+var drawing = false;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     strokeWeight(8);
@@ -5,10 +10,20 @@ function setup() {
 }
 
 function mouseDragged() {
-    stroke(162, 250, 163);
+    stroke(R, G, B);
     line(pmouseX, pmouseY, mouseX, mouseY);
+    if (!drawing) {
+        document.getElementById("footer").style.opacity = "100";
+    }
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+}
+
+function changeColor() {
+    R = Math.floor(Math.random() * 255);
+    G = Math.floor(Math.random() * 255);
+    B = Math.floor(Math.random() * 255);
+    document.getElementById("color-circle").style.backgroundColor = `rgb(${R},${G},${B})`;
 }
