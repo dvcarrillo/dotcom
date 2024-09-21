634743a6132990ca9111fbe374087461c910a1c1
David Vargas Carrillo <dv.carrillo@yahoo.com>
2024-09-21 20:57:58 +0200

    Dark theme added

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// Set the initial color scheme
+window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? document.documentElement.setAttribute('data-theme', 'dark') : document.documentElement.setAttribute('data-theme', 'light');
+
+// Watch for changes on color scheme
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    event.matches ? document.documentElement.setAttribute('data-theme', 'dark') : document.documentElement.setAttribute('data-theme', 'light');
+});
