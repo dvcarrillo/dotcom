b90aa4add171cbfca17d696d7dbdca4a302d5c42
David Vargas Carrillo <dv.carrillo@yahoo.com>
2025-03-22 16:43:00 +0100

    Added buttons and icons

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,8 @@ window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ?
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
     event.matches ? document.documentElement.setAttribute('data-theme', 'dark') : document.documentElement.setAttribute('data-theme', 'light');
 });
+
+// Load Feather icons
+document.addEventListener("DOMContentLoaded", function () {
+    feather.replace();
+  });
